31c1b0cf5a52fede7ea73a3c96ab68f270fa34ce
Taluama Velludo <taluama@gmail.com>
2019-07-30 13:05:12 -0400

    user model

--- models/User.js
+++ models/User.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema - new Schema({
+const userSchema = new Schema({
   username: String,
   password: String,
 });
 
 
-const User = mongoose.model('User');
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
