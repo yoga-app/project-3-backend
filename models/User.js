1bf9923472f251414573c727df521e5e034c510f
arstrel <arstrel@gmail.com>
2019-07-31 13:03:27 -0400

    added new user routes

--- models/User.js
+++ models/User.js
@@ -4,10 +4,10 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   username: String, //email
   password: String,
-  level: {type: String, enum:['yogi', 'guruji']},
+  level: {type: String, enum:['yogi', 'guruji'], default: 'yogi'},
   firstName: String,
   lastName: String,
-  picture: String, //url to claudinary 
+  picture: {type: String, default: 'https://res.cloudinary.com/ironhack55/image/upload/v1564588169/default-profile_tlj5l0.png'}, //url to claudinary 
   subscription: {
                   startedDate: Date,
                   endDate: Date,
@@ -15,7 +15,12 @@ const userSchema = new Schema({
   package: {
             classesLeft: Number,
             type: {type: String, enum: ['beginner', 'medium', 'advanced']}
-            },
+            }
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
 });
 
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,10 +3,12 @@ const router    = express.Router();
 const User      = require('../models/User');
 const bcrypt    = require('bcryptjs');
 const passport  = require('passport');
+const uploadMagic = require('../config/cloudinary');
 
 router.post('/signup', (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
+  
 
   if (!username || !password) {
     res.status(400).json({ message: 'Provide username and password' });
@@ -37,7 +39,10 @@ router.post('/signup', (req, res, next) => {
 
       const aNewUser = new User({
           username:username,
-          password: hashPass
+          password: hashPass,
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          
       });
 
       aNewUser.save(err => {
@@ -91,6 +96,12 @@ router.get('/getcurrentuser', (req, res, next) => {
         // add first name and last name to use in welcome message
         newObject.username = req.user.username;
         newObject._id = req.user._id;
+        newObject.firstName = req.user.firstName;
+        newObject.lastName = req.user.lastName;
+        newObject.picture = req.user.picture;
+        newObject.subscription = req.user.subscription;
+        newObject.package = req.user.package;
+        newObject.created_at = req.user.created_at;
 
         res.status(200).json(newObject);
         return;
@@ -98,5 +109,45 @@ router.get('/getcurrentuser', (req, res, next) => {
     res.status(403).json({ message: 'Unauthorized' });
 });
 
+router.post('/updateuserinfo/:id', uploadMagic.single('image'), (req, res, next) => {
+    if(req.file.url) {req.body.picture = req.file.url}
+    User.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then((updatedUser)=> {
+        res.json({updatedUser: updatedUser, message: 'User info updated successfully'})
+    })
+    .catch(err=> {
+        res.status(500).json({message: `Something went wrong updating user`})
+    })
+})
+
+router.post('/deleteprofile/:id', (req, res, next) => {
+    User.findByIdAndRemove(req.params.id)
+    .then(()=> {
+        res.status(200).json({message: `Profile deleted`})
+    })
+    .catch(err=> {
+        res.status(500).json({message: `Something went wrong deleting the profile`})
+    })
+})
+
+router.get('/getuserbyid/:id', (req, res, next) => {
+    User.findById(req.params.id)
+    .then((user)=> {
+        res.json(user)
+    })
+    .catch(err=> {
+        res.status(500).json({message: 'Something went wrong finding user by id'})
+    })
+})
+
+router.get('/getallusers', (req, res, next) => {
+    User.find()
+    .then(allUsers => {
+        res.json(allUsers)
+    })
+    .catch(err => {
+        res.status(500).json({message: 'Something went wrong geting all users'})
+    })
+})
 
 module.exports = router;
