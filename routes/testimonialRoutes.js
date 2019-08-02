89ab3d58e082e2256c4da859af2fe299f1d9eea0
arstrel <arstrel@gmail.com>
2019-08-02 14:52:08 -0400

    testimonials seed route

--- routes/testimonialRoutes.js
+++ routes/testimonialRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router  = express.Router();
 const Testimonial = require('../models/Testimonial');
 const uploadMagic = require('../config/cloudinary');
+const goodies = require('../bin/testimonials.json');
 
 
 /* GET home page */
@@ -46,6 +47,21 @@ router.post('/updatebyid/:id', uploadMagic.single('picture'), (req, res, next) =
   .catch(err =>{
     res.status(500).json({message: 'Something went wrong updating testimonial'})
   })
+})
+
+
+router.post('/data/seed/testimonials', (req, res, next) => {
+  if(req.body.key == "coolboy55") {
+    Testimonial.create(goodies)
+    .then((response)=> {
+      res.json(`Database seeded successfully`)
+    })
+    .catch(err=> {
+      res.json(err)
+    })
+  } else {
+    res.status(403).json('Invalid credentials')
+  }
 })
 
 module.exports = router;
