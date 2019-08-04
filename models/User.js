6eaebea0b177d1fc05bb48c3b48cf1de588693e0
arstrel <arstrel@gmail.com>
2019-08-04 15:10:29 -0400

    added default gallery json, add isAdmin to user model

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userSchema = new Schema({
   username: String, //email
   password: String,
   level: {type: String, enum:['yogi', 'guruji'], default: 'yogi'},
+  isAdmin: {type: Boolean, default: false},
   firstName: String,
   lastName: String,
   picture: {type: String, default: 'https://res.cloudinary.com/ironhack55/image/upload/v1564588169/default-profile_tlj5l0.png'}, //url to claudinary 
